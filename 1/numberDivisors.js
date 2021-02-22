7332846bae3c62276bb3a53f5b718478eb30826e
Aleksandr Khorev <aleks.k@tonlabs.io>
2021-02-22 17:53:45 +0300

    fix comment

--- 1/numberDivisors.js
+++ 1/numberDivisors.js
@@ -2,15 +2,17 @@
 function numberDivisors(number) {
   const result = [];
   for (let divisor = 1; divisor < (number/divisor + 1); divisor++) {
-    if (number % divisor === 0 && !result[divisor]) { result.push(divisor); result.push(number/divisor);}
+    if (number % divisor === 0) { 
+      result.push(divisor);
+      divisor === number/divisor? {} : result.push(number/divisor);
+    }
   }
   return result;
 }
 
 
-
-
 console.log(numberDivisors(6)); // [1,6,2,3]
+console.log(numberDivisors(49)); // [ 1, 49, 7, 7]
 console.log(numberDivisors(1)); // [1, 1] a side effect for 1
 console.log(numberDivisors(12543)); // [ 1, 12543, 3, 4181, 37, 339, 111, 113]
 console.log(numberDivisors(543980)); // [1, 2, 4, 10, 20, 59, 118, 236, 295, 461, 590, 922, 1180, 1844, 2305, 4610, 9220, 27199, 54398, 135995, 271990, 543980] or originall 
