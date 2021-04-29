8077b132d850b9ebaf959bfdf15b1f7b9895a1f3
Aleksandr Khorev <aleks.k@tonlabs.io>
2021-04-29 21:31:40 +0300

    search in a Binary Search Tree

---
+++ leetcode/tree/SearchSubTree.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/search-in-a-binary-search-tree/
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        def search(node):
+            if (not node): return None
+            if (node.val == val):
+                return node
+            else:
+                leftSearch = search(node.left)
+                if (leftSearch): return leftSearch
+                rightSearch = search(node.right)
+                if (rightSearch): return rightSearch
+            
+        return search(root)
