5a46a1cd20bc645a1fb489959363cb664b7e747e
Aleksandr Khorev <aleks.k@tonlabs.io>
2021-11-07 16:24:37 +0300

    fifo implementation for stack (based on queue)

---
+++ stack/FirstIntoFirstOutStack.py
@@ -0,0 +1,46 @@
+class Element:
+  def __init__(self, value):
+    self.value = value
+    self.next:Element = None
+
+
+class Stack:
+    def __init__(self) -> None:
+      self.first:Element = None
+      self.last:Element = None
+
+    def push(self, element: Element):
+      if (self.first == None):
+          self.first = element
+          self.last = self.first
+
+      self.last.next = element
+      self.last = element
+
+    def pop(self):
+        if(self.first == None):
+            return None
+        result = self.first
+        self.first = self.first.next
+        return result
+
+    def isEmpty(self):
+       return (self.last == None)
+
+    def print(self):
+      element = self.first
+      while(element):
+          print(element.value)
+          element = element.next
+      print('--------')
+
+stack = Stack()
+stack.push(Element(2))
+stack.push(Element(3))
+stack.push(Element(4))
+stack.push(Element(5))
+stack.print()
+stack.pop()
+stack.print()
+print(stack.isEmpty())
+print(Stack().isEmpty())
