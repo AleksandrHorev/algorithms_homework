cfcc0f1fd25a8f7c4383f8bf48c25290c2e7f005
Aleksandr Khorev <aleks.k@tonlabs.io>
2021-12-07 20:05:19 +0300

    optimal solution for binary lits to int

--- leetcode/dynamic/BinaryListToInt.py
+++ leetcode/dynamic/BinaryListToInt.py
@@ -1,17 +1,8 @@
-# not optimal, can do without additional array
 class Solution:
     def getDecimalValue(self, head: ListNode) -> int:
-        node = head
-        arr = []
-        while(node):
-            arr.append(int(node.val))
-            node = node.next
         result = 0
-        nextNumber = 1
-        for el in arr[::-1] :
-            if (el != 0):
-                result += nextNumber
-            nextNumber = nextNumber * 2
+        while(head):
+            result = result * 2 + head.val
+            head = head.next
         
         return result
-        
